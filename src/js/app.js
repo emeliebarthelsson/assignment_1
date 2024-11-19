182b6c246c0d71d3fc6b5ac770237369a59d127f
Emelie Barthelsson <emeliebarthelsson@gmail.com>
2024-11-19 12:33:49 +0100

    create new navbar, add comments

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+const toggleMenuButton = document.querySelector(".navbar__toggle");
+const navbarLinksContainer = document.querySelector(".navbar__links");
+const links = document.querySelectorAll(".navbar__link");
+
+toggleMenuButton.addEventListener("click", () => {
+  navbarLinksContainer.classList.toggle("navbar__links--active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    links.forEach((link) => link.classList.remove("navbar__link--active")); // remove active class from all li
+
+    e.currentTarget.classList.add("navbar__link--active"); // identify clicked link
+  });
+});
